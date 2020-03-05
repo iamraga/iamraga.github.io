26f7e3b224f975ae3e75ec56bb14692bd6d13df7
Ragavendhar T <ragavendhar.t@zohocorp.com>
2020-03-06 00:39:01 +0530

    First commit with config

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Index() {
+    return (
+        <div>
+            <h1>Site under construction!</h1>
+            <h3>Trying all possible commands</h3>
+        </div>
+    )
+}
